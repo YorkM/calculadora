2a6d2aa41bfbea3602a2cafe0222685eeeadde93
206 <206@academicos.cesde.edu.co>
2022-04-22 20:42:51 -0500

    se crea metodo

---
+++ Calculadora.py
@@ -0,0 +1,10 @@
+class Calculadora:
+    def __init__(self,num1,num2):
+        self.num1 = num1
+        self.num2 = num2
+
+    def sumar(self):
+        suma = self.num1+self.num2
+        return(suma)
+
+        
